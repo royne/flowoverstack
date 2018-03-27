64b58756e63168186a90e419b168f6c6072cb9a1
royne <royne.chaves@gmail.com>
2018-03-27 09:20:31 -0500

    genera relacion polimorfica en comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323043211) do
+ActiveRecord::Schema.define(version: 20180327141621) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20180323043211) do
     t.integer "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "questions", force: :cascade do |t|
